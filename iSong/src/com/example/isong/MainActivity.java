24a620d8ecdeac164c6bec8bfb9e047fd0eb2e78
ellizzabbetth <z.z.bradley@gmail.com>
2014-01-15 06:21:58 -0500

    first commit

---
+++ iSong/src/com/example/isong/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.isong;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+//test
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
